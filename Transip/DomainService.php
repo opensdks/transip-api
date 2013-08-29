d787a30ef25d809251856943bf34b057d2b9b633
Martijn Gastkemper <mgastkemper@adamsyoung.nl>
2013-08-29 14:46:11 +0200

    Syntax error

--- Transip/DomainService.php
+++ Transip/DomainService.php
@@ -70,7 +70,7 @@ class Transip_DomainService
 				'trace'    => false, // can be used for debugging
 			);
 			
-			$options = array_merge( $options, Transip_ApiSettings::$soapOptions )
+			$options = array_merge( $options, Transip_ApiSettings::$soapOptions );
 
 			$wsdlUri  = "https://{$endpoint}/wsdl/?service=" . self::SERVICE;
 
